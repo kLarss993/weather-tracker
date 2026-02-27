5e430f92acb733360d27865676a98c3e7bc9ee85
klarss <timurandriuca6@gamil.com>
2026-02-28 00:32:46 +0200

    Added simply API logic

--- code/app.py
+++ code/app.py
@@ -1,6 +1,19 @@
 from flask import *
+import requests
 
 app = Flask(__name__)
+url = "https://api.open-meteo.com/v1/forecast"
+
+params = {
+    "latitude": 50.45,
+    "longitude": 30.52,
+    "current_weather": True
+}
+
+resp = requests.get(url, params=params)
+data = resp.json()
+
+print(data)
 
 
 @app.route('/')
