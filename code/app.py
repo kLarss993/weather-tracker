12faea65fd3ddfd8dcf44266713b7a544066722e
klarss <timurandriuca6@gamil.com>
2026-02-28 22:43:59 +0200

    Enhance home page functionality to display weather data and improve city input handling

--- code/app.py
+++ code/app.py
@@ -1,4 +1,5 @@
 from flask import *
+from datetime import *
 import requests
 
 app = Flask(__name__)
@@ -8,6 +9,8 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 @app.route('/home', methods=['GET', 'POST'])
 def home():
+    now = datetime.now()
+    
     temp = None
     wndspd = None
     error = None
@@ -16,9 +19,12 @@ def home():
     if request.method == 'POST':
         city = request.form['city']
 
+
         if not city:
             error = "Please enter a city name."
         else:
+            city=city.capitalize()
+
             geo_url = "https://geocoding-api.open-meteo.com/v1/search"
             geo_params = {
                 "name": city,
@@ -27,12 +33,15 @@ def home():
                 "format": "json"
             }
 
-        city=city.capitalize()
-
         geo_data = requests.get(geo_url, params=geo_params).json()
+
+        if not geo_data.get("results"):
+            error = "City not found."
+            return render_template("home.html", error=error)
 
         latitude = geo_data['results'][0]['latitude']
         longitude = geo_data['results'][0]['longitude']
+
 
         weath_url = "https://api.open-meteo.com/v1/forecast"
 
@@ -49,7 +58,35 @@ def home():
         city = geo_data['results'][0]['name']
 
 
+        url = "https://api.open-meteo.com/v1/forecast"
+
+        params = {
+            "latitude": latitude,
+            "longitude": longitude,
+            "hourly": ["temperature_2m", "windspeed_10m"],
+            "timezone": "Europe/Kyiv"
+        }
+
+        data = requests.get(url, params=params).json()
+
+        times = data["hourly"]["time"]
+        temps = data["hourly"]["temperature_2m"]
+        winds = data["hourly"]["windspeed_10m"]
+
+        target_date = "2026-03-01"
+
+        result = []
+
+        for i, t in enumerate(times):
+            if t.startswith(target_date):
+                result.append((t, temps[i], winds[i]))
+
+        print(result)
+
+
     return render_template('home.html',
+                           now=now,
+                           result=result,
                            temp=temp,
                            wndspd=wndspd,
                            error=error,
