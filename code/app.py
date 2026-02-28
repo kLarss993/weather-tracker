7cd01b9f3341745d5339a2eaa8265e0aa5f6ef39
klarss <timurandriuca6@gamil.com>
2026-02-28 11:30:41 +0200

    Added some fuctions

--- code/app.py
+++ code/app.py
@@ -5,26 +5,59 @@ app = Flask(__name__)
 
 
 
-@app.route('/')
-@app.route('/home')
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def home():
-    url = "https://api.open-meteo.com/v1/forecast"
+    temp = None
+    wndspd = None
+    error = None
 
-    params = {
-        "latitude": 50.45,
-        "longitude": 30.52,
-        "current_weather": True
-    }
+    if request.method == 'POST':
+        city = request.form['city']
 
-    resp = requests.get(url, params=params)
-    data = resp.json()
+        if not city:
+            error = "Please enter a city name."
+        else:
+            geo_url = "https://geocoding-api.open-meteo.com/v1/search"
+            geo_params = {
+                "name": city,
+                "count": 1,
+                "language": "en",
+                "format": "json"
+            }
 
-    temp = data['current_weather']['temperature']
-    wndspd = data['current_weather']['windspeed']
+        geo_url = "https://geocoding-api.open-meteo.com/v1/search"
+
+        geo_params = {
+            "name": city,
+            "count": 1,
+            "languages": "en,ua",
+            "format": "json"
+        }
+
+        geo_data = requests.get(geo_url, params=geo_params).json()
+
+        latitude = geo_data['results'][0]['latitude']
+        longitude = geo_data['results'][0]['longitude']
+
+        weath_url = "https://api.open-meteo.com/v1/forecast"
+
+        weath_params = {
+            "latitude": latitude,
+            "longitude": longitude,
+            "current_weather": True
+        }
+
+        weath_data = requests.get(weath_url, params=weath_params).json()
+
+        temp = weath_data['current_weather']['temperature']
+        wndspd = weath_data['current_weather']['windspeed']
+
 
     return render_template('home.html',
                            temp=temp,
-                           wndspd=wndspd)
+                           wndspd=wndspd,
+                           error=error)
 
 
 if __name__ == '__main__':
